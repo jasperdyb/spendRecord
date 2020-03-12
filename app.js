3d5f0108a8782ef465ca35f7a5dd86c2451508f9
Jasper Chen <jasperdyb@gmail.com>
2020-03-12 21:30:03 +0800

    Project initialize
    set up express app , home page,

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+
+
+const port = 3000
+
+//middleware
+app.set('view engine', 'pug')
+
+//routes
+app.use('/', require('./routes/home.js'))
+
+
+app.listen(port)
