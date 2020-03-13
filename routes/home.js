ca55131b678cb2793d36dffb4a08956d92db118f
Jasper Chen <jasperdyb@gmail.com>
2020-03-13 22:28:31 +0800

    Fix filter function
    + Add message while there's no record

--- routes/home.js
+++ routes/home.js
@@ -6,12 +6,14 @@ router.get('/', (req, res, next) => {
   let category = 'default'
   let method = {}
 
-  if (req.query) {
+  if (req.query.category) {
+    console.log(req.query)
     category = req.query.category
   }
 
   if (category != 'default') {
     method = { category: category }
+    console.log(category, method)
   }
 
 
@@ -25,7 +27,8 @@ router.get('/', (req, res, next) => {
       for (var i = 0; i < records.length; i++) {
         totalAmount += records[i].amount
       }
-
+      // console.log(records)
+      //TODO Add message to note there's no record in the account or category
       return res.render('index', { records, totalAmount, category }) // 將資料傳給 index 樣板
     })
 })
